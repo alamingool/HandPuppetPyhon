d43ce18c97991563d91b8a8c37e69e5c91d88800
alamingool <williamhuynh1610@gmail.com>
2025-04-28 01:33:58 +0700

    Upped complexity ->1

--- hand_tracking.py
+++ hand_tracking.py
@@ -125,7 +125,7 @@ def video_processing_loop(settings, frame_q, stop_flag):
     hands = mp_hands.Hands(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5,
-        model_complexity=0,
+        model_complexity=1,
         static_image_mode=False,
         max_num_hands=2
     )
